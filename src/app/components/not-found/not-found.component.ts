3b072f808350f1131e9933462eb49ad78aead459
xSam08 <samoluo8@gmail.com>
2024-12-12 12:00:10 -0500

    Partially finished Footer and minor changes, finished Not Found Component.

--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css'
 })
