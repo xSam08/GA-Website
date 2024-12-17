dd2db3f56bd2d23cc0a9a86ca911b37d2890ea40
xSam08 <samoluo8@gmail.com>
2024-12-16 22:11:27 -0500

    Finished the Layout, styles and reactiveness of the Contact Form. Pending the email sending logic.

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,11 +1,64 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { 
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    CommonModule
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
 
+  contactForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      firstName: [
+        '', [
+          Validators.required,
+          Validators.minLength(3)
+        ]
+      ],
+      secondName: [
+        '', [
+          Validators.required,
+          Validators.minLength(3)
+        ]
+      ],
+      email: [
+        '', [
+          Validators.required,
+          Validators.email
+        ]
+      ],
+      subject: [
+        '', [
+          Validators.required,
+          Validators.minLength(10)
+        ]
+      ],
+      message: [
+        '', [
+          Validators.required,
+          Validators.minLength(30)
+        ]
+      ]
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log('Formulario válido:', this.contactForm.value);
+    }
+  }
 }
