cf2c15615abbda16d2daf8922881790d275d7255
xSam08 <samoluo8@gmail.com>
2024-12-25 13:48:28 -0500

    Fixed and improved button to change languages in desk and mobile devices

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,9 +8,17 @@ import { RouterModule } from '@angular/router';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  lang = 'es';
 
-  changeLang() {
-    this.lang === 'es' ? this.lang = 'en' : this.lang = 'es';
+  public lang: string = 'es';
+  public isDropdownVisible: boolean = false;
+
+  toggleDropdown() {
+    this.isDropdownVisible = !this.isDropdownVisible;
+  }
+
+  changeLang(event: MouseEvent) {
+    event.stopPropagation();
+    this.lang = this.lang === 'es' ? 'en' : 'es';
+    this.isDropdownVisible = false;
   }
 }
